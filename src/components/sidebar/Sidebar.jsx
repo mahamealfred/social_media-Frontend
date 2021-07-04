8cbada4cd37cf691bed17ff67cf99ea43095a315
mahamealfred <mahamealfred@gmail.com>
2021-07-04 20:10:00 +0200

    Change on home page

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,7 @@
 import "./sidebar.css";
-import {RssFeed,HelpOutline,WorkOutline,Event,School,PlayCircleFilledOutlined,Group,Bookmark,Message} from "@material-ui/icons"
+import {RssFeed,HelpOutline,WorkOutline,Event,School,PlayCircleFilledOutlined,Group,Bookmark,Message} from "@material-ui/icons";
+import {Users} from "../../dummyData";
+import CloseFriend from "../closeFriend/CloseFriend";
 
 export default function Sidebar() {
     return (
@@ -55,34 +57,10 @@ export default function Sidebar() {
                <button className="sidebarButton"> Show More</button> 
                <br className="sidebarHr"/>
                <ul className="sidebarFriendList">
-                   <li className="sidebarFriend">
-                       <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt=""/>
-                       <span className="sidebarFriendName">Mahame alfred</span>
-                   </li>
-                   <li className="sidebarFriend">
-                       <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt=""/>
-                       <span className="sidebarFriendName">Mahame alfred</span>
-                   </li>
-                   <li className="sidebarFriend">
-                       <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt=""/>
-                       <span className="sidebarFriendName">Mahame alfred</span>
-                   </li>
-                   <li className="sidebarFriend">
-                       <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt=""/>
-                       <span className="sidebarFriendName">Mahame alfred</span>
-                   </li>
-                   <li className="sidebarFriend">
-                       <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt=""/>
-                       <span className="sidebarFriendName">Mahame alfred</span>
-                   </li>
-                   <li className="sidebarFriend">
-                       <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt=""/>
-                       <span className="sidebarFriendName">Mahame alfred</span>
-                   </li>
-                   <li className="sidebarFriend">
-                       <img className="sidebarFriendImg" src="/assets/person/2.jpeg" alt=""/>
-                       <span className="sidebarFriendName">Mahame alfred</span>
-                   </li>
+                   {Users.map((u) =>(
+                       <CloseFriend key={u.id} user={u}/>
+                   ))}
+                  
                </ul>
             </div>
         </div>
